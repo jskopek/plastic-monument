26b2479feb2d195f095e58cc7c50cc7c8484d2e5
Jean-Marc Skopek <jskopek@gmail.com>
2019-10-18 17:48:29 -0700

    Experimenting with background image

--- js/code.js
+++ js/code.js
@@ -1,65 +1,95 @@
-// Our Javascript will go here.
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+var camera, scene, renderer, controls;
+var lon = 0, lat = 0, phi = 0, theta = 0;
+
+init();
+animate();
+
+function init() {
+
+    var container, mesh;
+
+    container = document.getElementById( 'container' );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
 
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
+    //camera.target = new THREE.Vector3( 0, 0, 0 );
+    camera.position.x = -2;
+    camera.position.y = 0;
+    camera.position.z = 1;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+    controls = new OrbitControls(camera);
+    controls.autoRotate = true;
 
+    scene = new THREE.Scene();
 
-function generateYear(width, index, humanMass, plasticMass) {
-    var height = humanMass + plasticMass
+    // loading background sphere
+    var geometry = new THREE.SphereGeometry( 50, 60, 40 );
+    geometry.scale( - 1, 1, 1 );
+    var texture = new THREE.TextureLoader().load( 'img/eight.jpg' )
+    var material = new THREE.MeshBasicMaterial({map: texture});
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
 
-    var humanMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var humanGeometry = new THREE.BoxGeometry( width, humanMass, 1 );
-    var humanCube = new THREE.Mesh( humanGeometry, humanMaterial );
-    //scene.add( humanCube );
-    humanCube.position.set(0, humanMass/2, 0);
+    for(var index = 0; index < 20; index++) {
+        let width = 2;
+        let humanMass = index + 1 /3
+        let plasticMass = index + 1
 
-    var plasticMaterial = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
-    var plasticGeometry = new THREE.BoxGeometry( width, plasticMass, 1 );
-    var plasticCube = new THREE.Mesh( plasticGeometry, plasticMaterial );
-    //scene.add( plasticCube );
-    plasticCube.position.set(0, humanMass + 0.1 +  plasticMass/2, 0);
+        let year = generateYear(width, index, humanMass, plasticMass);
+        year.position.set(-20 + (width + 2 * index), -30, 0)
+        scene.add(year)
 
-    var group = new THREE.Group()
-    group.add(humanCube)
-    group.add(plasticCube)
-    return group
+    }
 }
-
-for(var index = 0; index < 40; index++) {
-    let width = 2;
-    let humanMass = index + 1 /3
-    let plasticMass = index + 1
-
-    let year = generateYear(width, index, humanMass, plasticMass);
-    year.position.set(width + 2 * index, 0, 0)
-    scene.add(year)
-
-}
-
-//camera.position.z = 5;
-
-var controls = new OrbitControls( camera, renderer.domElement );
-controls.autoRotate = true;
-
-//controls.update() must be called after any manual changes to the camera's transform
-camera.position.set( 0, 0, 5 );
-controls.update();
 
 function animate() {
-    //cube.rotation.x += 0.01;
-    //cube.rotation.y += 0.01;
-
-    controls.update();
-
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+    controls.update()
+    //update();
 }
-animate();
+
+function update() {
+    lon += 0.1;
+    lat = Math.max( - 85, Math.min( 85, lat ) );
+    phi = THREE.Math.degToRad( 90 - lat );
+    theta = THREE.Math.degToRad( lon );
+
+    camera.target.x = 500 * Math.sin( phi ) * Math.cos( theta );
+    camera.target.y = 500 * Math.cos( phi );
+    camera.target.z = 500 * Math.sin( phi ) * Math.sin( theta );
+
+    camera.lookAt( camera.target );
+    renderer.render( scene, camera );
+}
+
+    function generateYear(width, index, humanMass, plasticMass) {
+        var height = humanMass + plasticMass
+
+        var humanMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        var humanGeometry = new THREE.BoxGeometry( width, humanMass, 1 );
+        var humanCube = new THREE.Mesh( humanGeometry, humanMaterial );
+        //scene.add( humanCube );
+        humanCube.position.set(0, humanMass/2, 0);
+
+        var plasticMaterial = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+        var plasticGeometry = new THREE.BoxGeometry( width, plasticMass, 1 );
+        var plasticCube = new THREE.Mesh( plasticGeometry, plasticMaterial );
+        //scene.add( plasticCube );
+        plasticCube.position.set(0, humanMass + 0.1 +  plasticMass/2, 0);
+
+        var group = new THREE.Group()
+        group.add(humanCube)
+        group.add(plasticCube)
+        return group
+    }
+
+
