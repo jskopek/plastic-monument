12b16b56ad66f33ec7a7002166c2c9e5fb0f94ef
Jean-Marc Skopek <jskopek@gmail.com>
2019-10-18 23:46:22 -0700

    Refactoring initialization and rendering

--- js/code.js
+++ js/code.js
@@ -3,64 +3,67 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import {Monument, scaleGroupChildren} from './monument.js';
 
 
-function init(containerEl) {
-    let renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    containerEl.appendChild( renderer.domElement );
+// initialize renderer
+let renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
 
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
-    //camera.target = new THREE.Vector3( 0, 0, 0 );
-    camera.position.x = -50;
-    camera.position.y = -47;
-    camera.position.z = -50;
-    camera.lookAt(0,-50,0);
+// add renderer to dom
+let containerEl = document.getElementById( 'container' )
+containerEl.appendChild( renderer.domElement );
 
-    let controls = new OrbitControls(camera);
-    controls.autoRotate = true;
+// initialize camera
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
+//camera.target = new THREE.Vector3( 0, 0, 0 );
+camera.position.x = -50;
+camera.position.y = -47;
+camera.position.z = -50;
+camera.lookAt(0,-50,0);
 
-    let scene = new THREE.Scene();
+// initialize orbital rotation
+let controls = new OrbitControls(camera);
+controls.autoRotate = true;
 
-    // create background sphere
-    let geometry = new THREE.SphereGeometry( 100, 60, 40 );
-    geometry.scale( - 1, 1, 1 );
-    let texture = new THREE.TextureLoader().load( 'img/four.jpg' )
-    let material = new THREE.MeshBasicMaterial({map: texture});
-    let mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+// initialize scene
+let scene = new THREE.Scene();
 
-    // create floor
-    let floorGeometry = new THREE.BoxGeometry(200,1,200);
-    let floorMaterial = new THREE.MeshBasicMaterial({color:0x000000})
-    let floorCube = new THREE.Mesh(floorGeometry, floorMaterial);
-    floorCube.position.set(0,-50,0);
-    scene.add(floorCube);
+// create background sphere
+let geometry = new THREE.SphereGeometry( 100, 60, 40 );
+geometry.scale( - 1, 1, 1 );
+let texture = new THREE.TextureLoader().load( 'img/four.jpg' )
+let material = new THREE.MeshBasicMaterial({map: texture});
+let mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
 
-    // generate a monument
-    let monument = new Monument(10);
-    let monumentGroup = monument.generateGroup()
-    monumentGroup.position.set(0,-50,0);
-    scene.add(monumentGroup);
+// create floor
+let floorGeometry = new THREE.BoxGeometry(200,1,200);
+let floorMaterial = new THREE.MeshBasicMaterial({color:0x000000})
+let floorCube = new THREE.Mesh(floorGeometry, floorMaterial);
+floorCube.position.set(0,-50,0);
+scene.add(floorCube);
 
-    return {renderer, controls, monument, scene, camera, monumentGroup}
-}
+// generate a monument
+let monument = new Monument(10);
+let monumentGroup = monument.generateGroup()
+monumentGroup.position.set(0,-50,0);
+scene.add(monumentGroup);
 
-function animate(initVals) {
-    requestAnimationFrame(() => { animate(initVals); });
+//let initVals = {renderer, controls, monument, scene, camera, monumentGroup}
+let scale = scaleGroupChildren(monumentGroup, 100, 2);
 
-    initVals.renderer.render( initVals.scene, initVals.camera );
-    initVals.controls.update()
-    initVals.monument.update();
+// looping animation method
+function animate() {
+    requestAnimationFrame(animate);
 
-    if(initVals.scale) {
-        initVals.scale();
+    renderer.render( scene, camera );
+    controls.update()
+
+    if(scale) {
+        scale();
     }
 }
-
-let initVals = init(document.getElementById( 'container' ));
-initVals.scale = scaleGroupChildren(initVals.monumentGroup, 100, 2);
-animate(initVals);
+animate();
 
 
-window.initVals = initVals
-window.scaleGroupChildren = scaleGroupChildren
+// DEBUG VALUES
+window.debug = {renderer, controls, monument, scene, camera, monumentGroup, scale}
