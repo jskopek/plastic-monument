0a2cd7c536bdcae629afd6d6d864ed91d2b0c2a5
Jean-Marc Skopek <jskopek@gmail.com>
2020-04-12 15:51:46 -0700

    Experimenting with older stuff

--- js/code.js
+++ js/code.js
@@ -5,16 +5,37 @@ import Monument from './monument.js';
 import Pillar from './pillar.js';
 import Scroller from './scroller.js'
 import { CountUp } from 'countup.js'
+import _ from 'lodash';
 const pillarData = require('../data.csv')
 require('../css/main.scss')
 // import ScaleGroupChildren from './scalegroupchildren.js';
 
 
-var scroller = new Scroller(document.getElementById('scroller'));
-pillarData.forEach((pillarRow) => { scroller.add(pillarRow.year, 0.05, true); })
-scroller.on('scroll', (year, index) => {
+var yearScroller = new Scroller(document.getElementById('year-scroller'));
+pillarData.forEach((pillarRow) => { yearScroller.add(pillarRow.year, 0.05, false); })
+yearScroller.on('scroll', (year, index) => {
+    var timelineIndex = parseInt((year - pillarData[0].year) / 10)
+    timeline.el.querySelectorAll('div').forEach((el, index) => {
+        el.dataset['activeDistance'] = index - timelineIndex;
+    });
+
+    console.log(year, timelineIndex)
     setActivePillar(index);
 });
+
+var timeline = new Scroller(document.getElementById('timeline'));
+var timelineValues = ['Past', '1950s', '1960s', '1970s', '1980s', '1990s', '2000s', '2010s', 'Today', 'Future', '2020s', '2030s', '2040s', '2050s']
+timelineValues.forEach((index) => { timeline.add(index, .04, true); })
+
+timeline.on('click', (year, index) => {
+    let yearScrollerIndex = yearScroller.indexOfId(String(year))
+    yearScroller.scrollTo(yearScrollerIndex == -1 ? 0 : yearScrollerIndex);
+    // timeline.el.querySelectorAll('div').forEach((el, index) => {
+    //     console.log(el, index);
+    // });
+    // console.log('timeline.index', index)
+});
+
 
 // initialize renderer
 let renderer = new THREE.WebGLRenderer();
@@ -42,6 +63,7 @@ camera.lookAt(lookAt)
 let controls = new OrbitControls(camera, containerEl);
 controls.autoRotate = false;
 controls.enableZoom = false;
+controls.enableRotate = false;
 //camera.zoom = 20;
 controls.rotateSpeed = 2.0
 controls.zoomSpeed = 5
@@ -126,23 +148,23 @@ function setActivePillar(index) {
 function getActivePillarIndex() { return window.activePillarIndex || 0; }
 // end animation helper functions
 
-// handling prev/next
-let playStepInterval = 6000
+// // handling prev/next
+// let playStepInterval = 6000
 
-function play()  {
-    window.playInterval = setInterval(() => {
-        if(getActivePillarIndex() < (monument.pillars.length - 1)) {
-            setActivePillar(getActivePillarIndex() + 1)
-        } else {
-            setActivePillar(0)
-        }
-    }, playStepInterval);
-}
-function pause()  {
-    clearInterval(window.playInterval);
-    window.playInterval = undefined
-}
-play();
+// function play()  {
+//     window.playInterval = setInterval(() => {
+//         if(getActivePillarIndex() < (monument.pillars.length - 1)) {
+//             setActivePillar(getActivePillarIndex() + 1)
+//         } else {
+//             setActivePillar(0)
+//         }
+//     }, playStepInterval);
+// }
+// function pause()  {
+//     clearInterval(window.playInterval);
+//     window.playInterval = undefined
+// }
+// play();
 
 // DEBUG VALUES
 //window.debug = {renderer, monument, scene, camera, monumentGroup, THREE, scroller}
