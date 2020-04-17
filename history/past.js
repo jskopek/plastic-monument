839502754e88120082c1c6513d665c208ab59068
Jean-Marc Skopek <jskopek@gmail.com>
2020-04-17 15:44:25 -0700

    Moving old threejs json objects

--- history/past.js
+++ history/past.js
@@ -6,7 +6,7 @@ import _ from 'lodash';
 var scrollMonitor = require('scrollmonitor');
 // require('../css/main.scss')
 require('./past.scss')
-var appJSON = require('./past-simple.json')
+var appJSON = require('./past.json')
 
 
 window.THREE = THREE; // Used by APP Scripts.
